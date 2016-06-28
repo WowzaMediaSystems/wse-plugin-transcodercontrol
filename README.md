691281f6f3b1c6fdaa2747aeda4d5e54086bbddc
Matt Young <myoung@wowza.com>
2016-06-28 07:17:44 -0400

    Initial commit for ModuleTranscoderControl

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# wse-plugin-transcodercontrol
+# Transcodercontrol
+The **ModuleTranscoderControl** module for [Wowza Streaming Engine™ media server software](https://www.wowza.com/products/streaming-engine) enables you to control which streams get transcoded by the Wowza Transcoder.
+
+## Prerequisites
+Wowza Streaming Engine 4.0.0 or later is required.
+
+## Usage
+This module provides the following functionality:
+
+* Whitelist which streams can be transcoder.
+* Blacklist which streams that should not be transcoded.
+* Use wildcard for multiple streams.
+
+## More resources
+[Wowza Streaming Engine Server-Side API Reference](https://www.wowza.com/resources/WowzaStreamingEngine_ServerSideAPI.pdf)
+
+[How to extend Wowza Streaming Engine using the Wowza IDE](https://www.wowza.com/forums/content.php?759-How-to-extend-Wowza-Streaming-Engine-using-the-Wowza-IDE)
+
+Wowza Media Systems™ provides developers with a platform to create streaming applications and solutions. See [Wowza Developer Tools](https://www.wowza.com/resources/developers) to learn more about our APIs and SDK.
+
+To use the compiled version of this module, see [How to control which streams get transcoded (ModuleTranscoderControl)](https://www.wowza.com/forums/content.php?588-How-to-control-which-streams-get-transcoded-%29ModuleTranscoderControl%29).
+
+## Contact
+[Wowza Media Systems, LLC](https://www.wowza.com/contact)
+
+## License
+This code is distributed under the [Wowza Public License](https://github.com/WowzaMediaSystems/wse-plugin-transcodercontrol/blob/master/LICENSE.txt).
---
+++ com/wowza/wms/plugin/TranscoderControl.java
@@ -0,0 +1,68 @@
+package com.wowza.wms.plugin;
+
+import com.wowza.util.StringUtils;
+import com.wowza.wms.application.IApplicationInstance;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.module.ModuleBase;
+import com.wowza.wms.stream.IMediaStream;
+import com.wowza.wms.stream.livetranscoder.ILiveStreamTranscoderControl;
+
+public class TranscoderControl extends ModuleBase
+{
+
+	private class Controller implements ILiveStreamTranscoderControl
+	{
+
+		public boolean isLiveStreamTranscode(String transcoder, IMediaStream stream)
+		{
+			if (stream.isTranscodeResult())
+			{
+				return false;
+			}
+
+			logger.info(MODULE_NAME + ".isLiveStreamTranscode [" + transcoder + " : " + stream.getName() + " : " + names + "]", stream);
+			if (names.equals("*"))
+			{
+				logger.info(MODULE_NAME + ".isLiveStreamTranscode [" + transcoder + " : " + stream.getName() + " :  names is wildcard. returning " + matchAllow + "]", stream);
+				return matchAllow;
+			}
+			if (StringUtils.isEmpty(names))
+			{
+				logger.info(MODULE_NAME + ".isLiveStreamTranscode [" + transcoder + " : " + stream.getName() + " : names is empty. returning " + noMatchAllow + "]", stream);
+				return noMatchAllow;
+			}
+
+			String[] namesArray = names.split(",");
+			for (String name : namesArray)
+			{
+				if (stream.getName().matches(name.trim()))
+				{
+					logger.info(MODULE_NAME + ".isLiveStreamTranscode [" + transcoder + " : " + stream.getName() + " : match found : " + name.trim() + " : returning " + matchAllow + "]", stream);
+					return matchAllow;
+				}
+			}
+			logger.info(MODULE_NAME + ".isLiveStreamTranscode [" + transcoder + " : " + stream.getName() + " : No match found : returning " + noMatchAllow + "]", stream);
+			return noMatchAllow;
+		}
+	}
+
+	public static final String MODULE_NAME = "TranscoderControl";
+	public static final String PROP_NAME_PREFIX = "transcoderControl";
+	
+	private WMSLogger logger;
+	private String names = "*";
+	private boolean matchAllow = true;
+	private boolean noMatchAllow = false;
+
+	public void onAppStart(IApplicationInstance appInstance)
+	{
+		logger = WMSLoggerFactory.getLoggerObj(appInstance);
+		
+		names = appInstance.getProperties().getPropertyStr(PROP_NAME_PREFIX + "Names", names);
+		matchAllow = appInstance.getProperties().getPropertyBoolean(PROP_NAME_PREFIX + "MatchAllow", matchAllow);
+		noMatchAllow = appInstance.getProperties().getPropertyBoolean(PROP_NAME_PREFIX + "NoMatchAllow", noMatchAllow);
+
+		appInstance.setLiveStreamTranscoderControl(new Controller());
+	}
+}
